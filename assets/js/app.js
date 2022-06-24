552cf20ef81f08e50f6129bbd5961ed62b956705
emmanuel ike <emmanuelike995@gmail.com>
2022-06-24 15:59:16 -0700

    timer

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,49 +1,123 @@
-const stopwatchtimer = document.querySelector(".stopwatch-timer")
+// stopwatch variable Declaration
+const stopwatchTimer = document.querySelector('.stopwatch-timer')
 const stopwatchStartBtn = document.querySelector('.stopwatch-btn-Start')
 const stopwatchStopBtn = document.querySelector('.stopwatch-btn-Stop')
 const stopwatchResetBtn = document.querySelector('.stopwatch-btn-Reset')
 
+// Timer Variable Declaration
+const timerForm = document.querySelector('.timer-form')
+
+const timerInputMinutes = document.querySelector('#minutes')
+const timerInputSeconds = document.querySelector('#seconds')
+const timerInputMillisec = document.querySelector('#millisec')
+
+const timerDisplayMinutes = document.querySelector('#timer-display-minutes')
+const timerDisplaySeconds = document.querySelector('#timer-display-seconds')
+const timerDisplayMillisec = document.querySelector('#timer-display-millisec')
+
+
+
+const timerStartBtn = document.querySelector('.timer-btn-start')
+const timerStopBtn = document.querySelector('.timer-btn-stop')
+
+    // Declaratoin of variable getElementById timerInput
+      let Minutes = document.getElementById('minutes')
+      let Seconds = document.getElementById('seconds')
+      let Millisec = document.getElementById('millisec')
+
+    // Declaration of variable getElementId timerDisplay
+      let DisplayMinutes =  document.getElementById('timer-display-minutes')    
+      let DisplaySeconds =  document.getElementById('timer-display-seconds')    
+      let DisplayMillisec = document.getElementById('timer-display-millisec')
+
+    
+      // Store a Reference to a variable Timer
+    //let startTimer = null;
+
 let stopwatchMilli = 0
 let stopwatchSeconds = 0
-let stopwatchMintues = 0
+let stopwatchMinutes = 0
 let interval;
 
-function runStopWatch() {
-    stopwatchMilli += 100
+// if Statement Function
+function runStopWatch(){
+  stopwatchMilli += 100
 
-    if(stopwatchMilli > 999) {
-        stopwatchSeconds++
-        stopwatchMilli = 0
-    }
+  if(stopwatchMilli > 999){
+    stopwatchSeconds++
+    stopwatchMilli = 0
+  }
 
-    if(stopwatchSeconds > 59) {
-        stopwatchMintues++
-        stopwatchSeconds = 0
-    }
-     console.log(stopwatchMintues, stopwatchSeconds, stopwatchMilli)
-     stopwatchtimer.innerHTML = stopwatchMintues + ":" + stopwatchSeconds + ":" + stopwatchMilli
-   
-    }
+  if(stopwatchSeconds > 59) {
+    stopwatchMilli++
+    stopwatchSeconds = 0
+  }
+  console.log(stopwatchMinutes, stopwatchSeconds, stopwatchMilli)
+  stopwatchTimer.innerHTML = stopwatchMinutes  + ":" + stopwatchSeconds + ":" + stopwatchMilli
 
-    const startStopWatch = () => {
-        clearInterval(interval)
-        interval = setInterval(runStopWatch, 100)
-    }
-    stopwatchStartBtn.addEventListener("click" , startStopWatch)
+}
+// addEventLister Function/methods Timer
+const timerReplace = () => { 
+  var errormessage = "";
 
-    const stopStopWatch = () => {
-        clearInterval(interval)
-    }
-    stopwatchStopBtn.addEventListener("click" , stopStopWatch)
+ 
+  if (Minutes.value ==""){
+    errormessage += "Enter Mintues \n";
+    Minutes.style.borderColor = "Red"
+  }
+  if (Seconds.value ==""){
+    errormessage +="Enter Seconds \n";
+    Seconds.style.borderColor = "Red"
+  }
+  if (Millisec.value ==""){
+    errormessage += "Enter Millisec \n";
+    Millisec.style.borderColor = "Red"
+  }
+  if (errormessage != "" ){
+    alert("Can't Start an Empty Timer");
+  }
 
-    const resetStopWatch = () => {
+  timerDisplayMinutes.innerHTML =  timerInputMinutes.value;
+  timerDisplaySeconds.innerHTML =  timerInputSeconds.value;
+  timerDisplayMillisec.innerHTML =  timerInputMillisec.value;
+}
+timerStartBtn.addEventListener('click', timerReplace)
 
-        stopwatchMilli = 0
-        stopwatchSeconds = 0
-        stopwatchMintues = 0
+const timerReset = () => {
+  Minutes.value = 0 + '0'
+  Seconds.value = 0 + '0'
+  Millisec.value = 0 + '0'
 
-        stopwatchtimer.innerHTML = '00:00:00'
-        clearInterval(interval)
-    }
-    stopwatchResetBtn.addEventListener("click", resetStopWatch)
+  DisplayMinutes.innerHTML = 0 + '0'
+  DisplaySeconds.innerHTML = 0 + '0'
+  DisplayMillisec.innerHTML = 0 + '0'
+  
+  clearInterval(timerReplace)
+}
+timerStopBtn.addEventListener('click', timerReset)
+
+
+
+// addEventListen Function/methods Stopwatch
+
+const startStopWatch = () => {
+    clearInterval(interval)
+    interval = setInterval(runStopWatch, 100)
+}
+stopwatchStartBtn.addEventListener("click", startStopWatch)
+
+const stopStopWatch = () => {
+  clearInterval(interval)
+}
+stopwatchStopBtn.addEventListener("click", stopStopWatch)
+
+const resetStopWatch = () => {
+    stopwatchMilli = 0
+    stopwatchSeconds = 0
+    stopwatchMinutes = 0
+    
+    stopwatchTimer.innerHTML = '00:00:000'
+    clearInterval(interval)
+}
+stopwatchResetBtn.addEventListener("click", resetStopWatch)
     
